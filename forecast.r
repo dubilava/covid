67b6855f4ee43b96e2124a4ca0052167f6a76eca
David Ubilava <david.ubilava@sydney.edu.au>
2021-09-05 21:00:55 +1000

    updated code and graphs

--- forecast.r
+++ forecast.r
@@ -97,7 +97,6 @@ nsw_cb <- rbind(nsw_dt,nsw_fc)
 
 # melt to 'long table' for plotting purposes
 nsw_lg <- melt(nsw_cb,id.vars=c("date","lower","upper"))
-
 nsw_lg$variable <- factor(nsw_lg$variable,levels=c("observed","forecast"))
 
 # plot the graph
@@ -105,7 +104,7 @@ gg_plot <- ggplot(nsw_lg,aes(x=date,y=value))+
   geom_ribbon(aes(ymin=lower,ymax=upper),fill="steelblue",alpha=.25)+
   geom_line(aes(color=variable,linetype=variable),size=.5,na.rm=T)+
   scale_color_manual(values=c("darkgray","steelblue"))+
-  scale_linetype_manual(values=c(1,5,2,2))+
+  scale_linetype_manual(values=c(1,5))+
   labs(title="COVID NSW",subtitle=paste("Forecast made on",format(as.Date(max(covid_dt$date)),"%d %b %Y"),"for the subsequent seven days"),x="Date",y="Daily Cases",caption="Source of the data: https://www.covidaustralia.com/cases")+
   theme_classic()+
   theme(axis.title=element_text(size=12),axis.text=element_text(size=10),legend.title=element_blank(),legend.text=element_text(size=8),legend.position=c(.025,1),legend.justification=c(0,1),plot.caption = element_text(color="darkgray",size=9),plot.subtitle=element_text(size=9),legend.key.size=unit(0.5,"cm"))
@@ -131,16 +130,19 @@ yexp_f <- exp(coef(reg_exp)["a"]+trend_f*coef(reg_exp)["b"])
 
 # arrange the dataset to bind together the fitted and forecast values along with the observed data
 nsw_dt <- covid_dt[,.(date,observed=nsw,logistic=yhat_str,exponential=yhat_exp)]
-nsw_fc <- data.table(date=seq(covid_dt[date==max(date)]$date+1,by="day",length.out=7),observed=NA,logistic=ystr_f,exponential=yexp_f)
+nsw_dt[,`:=`(lower=NA,upper=NA)]
+nsw_fc <- data.table(date=seq(covid_dt[date==max(date)]$date+1,by="day",length.out=h),observed=NA,logistic=ystr_f,exponential=yexp_f,lower=ystr_ci$lower,upper=ystr_ci$upper)
 
 nsw_cb <- rbind(nsw_dt,nsw_fc) 
 
 # melt to 'long table' for plotting purposes
-nsw_lg <- melt(nsw_cb,id.vars="date")
+nsw_lg <- melt(nsw_cb,id.vars=c("date","lower","upper"))
+nsw_lg$variable <- factor(nsw_lg$variable,levels=c("observed","logistic","exponential"))
 
 # plot the graph
-gg_plot <- ggplot(nsw_lg,aes(x=date,y=value,color=variable,linetype=variable))+
-  geom_line(size=.5,na.rm=T)+
+gg_plot <- ggplot(nsw_lg,aes(x=date,y=value))+
+  geom_ribbon(aes(ymin=lower,ymax=upper),fill="steelblue",alpha=.25)+
+  geom_line(aes(color=variable,linetype=variable),size=.5,na.rm=T)+
   scale_color_manual(values=c("darkgray","steelblue","indianred"))+
   scale_linetype_manual(values=c(1,5,2))+
   labs(title="COVID NSW",subtitle=paste("Forecast made on",format(as.Date(max(covid_dt$date)),"%d %b %Y"),"for the subsequent seven days"),x="Date",y="Daily Cases",caption="Source of the data: https://www.covidaustralia.com/cases")+
@@ -149,57 +151,3 @@ gg_plot <- ggplot(nsw_lg,aes(x=date,y=value,color=variable,linetype=variable))+
 
 # save the figure
 ggsave("covid_models.png",dpi="retina",width=6.5,height=3.5)
-
-
-# # a pseudo-forecasting exercise to check how the model has been performing in previous several days
-# P <- 7
-# 
-# end_dates <- covid_dt$date[(length(covid_dt$date)-P-6):(length(covid_dt$date)-P)]
-# 
-# errors_mat <- matrix(NA,nrow=length(end_dates),ncol=h)
-# 
-# for(i in 1:P){
-#   
-#   # logistic smooth transition trend
-#   window_dt <- covid_dt[date<=as.Date(end_dates[i])]
-#   
-#   window_dt$G <- as.numeric((1+exp(-(2/sd(window_dt$trend))*(window_dt$trend-max(window_dt$trend))))^(-1))
-#   
-#   theta <- as.numeric(c(lm(nsw~G-1,data=window_dt)$coef,2,max(window_dt$trend)))
-#   theta <- 0.9*theta+rnorm(length(theta),0,0.01)
-#   names(theta) <- c("b","g","c")
-#   
-#   # fit the nonlinear least squares
-#   reg_str <- nlsLM(nsw~b*((1+exp(-g/sd(trend)*(trend-c)))^(-1)),data=window_dt,start=theta,lower=c(-Inf,.1,-Inf),upper=c(Inf,100,Inf),control=nls.control(maxiter=1000,warnOnly=T))
-#   
-#   
-#   # multi-day forecast
-#   trend_f <- c(as.numeric(max(window_dt$trend)+1):as.numeric(max(window_dt$trend)+h))
-#   
-#   Ghat_f <- ((1+exp(-coefficients(reg_str)["g"]/(sd(window_dt$trend))*(trend_f-coefficients(reg_str)["c"])))^(-1))
-#   
-#   ystr_f <- round(coefficients(reg_str)["b"]*Ghat_f)
-#   ystr_a <- covid_dt[date>as.Date(end_dates[i]) & date<=as.Date(end_dates[i])+h]$nsw
-#   
-#   errors_mat[i,] <- ystr_a-ystr_f
-#   
-# }
-# 
-# 
-# errors_dt <- as.data.table(abs(errors_mat))
-# colnames(errors_dt) <- c("one","two","three","four","five","six","seven")
-# errors_dt$date <- end_dates
-# 
-# errors_lg <- melt(errors_dt[,.(date,one_day_ahead=one,seven_days_ahead=seven)],id.vars="date")
-# 
-# gg_errors <- ggplot(errors_lg,aes(x=date,y=value,color=variable,linetype=variable))+
-#   geom_line(size=.6,na.rm=T)+
-#   scale_color_manual(values=c("darkgray","steelblue"))+
-#   scale_linetype_manual(values=c(1,5))+
-#   coord_cartesian(ylim=c(0,max(errors_lg$value)*1.5))+
-#   labs(title="COVID NSW",subtitle=paste("Forecasts made between",format(as.Date(end_dates[1]),"%d %b %Y"),"and",format(as.Date(end_dates[7]),"%d %b %Y")),x="Date",y="Absolute Forecast Error (Days)",caption="Source of the data: https://www.covidaustralia.com/cases")+
-#   theme_classic()+
-#   theme(axis.title=element_text(size=12),axis.text=element_text(size=10),legend.title=element_blank(),legend.text=element_text(size=9),legend.position=c(.025,1),legend.justification=c(0,1),plot.caption = element_text(color="darkgray",size=9),plot.subtitle=element_text(size=9))
-# 
-# # save the figure
-# ggsave("covid_errors.png",dpi="retina",width=6.5,height=3.5)
